3ffc766654971c320d2ee6fc9b1642df20209d44
rohitaryal <erucix@gmail.com>
2025-06-25 22:38:32 +0530

    Add types for user context

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,23 +1,27 @@
 import type { Tracker } from "./tracking.js";
 
-// Represents each user
-export interface AuthorizedUser {
+// Represents what routes and middlewares know about a clients request
+export interface UserContext {
     // Unique identifier of each user
-    username: string,
+    username: string;
     // Email address to identify the user
-    email: string,
+    email: string;
     // Hashed password of user
-    hashedPassword: string,
+    hashedPassword: string;
     // Link to user profile
-    avatar: string,
+    avatar: string;
     // UUID of file user has owned
-    userFiles: string[],
+}
+
+// Represents each user
+export interface AuthorizedUser extends UserContext {
+    userFiles: string[];
     // UUID of foles that user has shared publicly and is visible with link
-    publiclyShared: string[],
+    publiclyShared: string[];
     // UUID of files that user has publicly shared on pool and is visible on pool
-    publiclySharedInPool: string[],
+    publiclySharedInPool: string[];
     // UUID of files user has privately to some user
-    sharedPrivately: string[],
+    sharedPrivately: string[];
     // User tracking
-    tracking: Tracker,
+    tracking: Tracker;
 }
