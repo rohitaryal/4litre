dc9b12db1fad674b1ba3d2276cfd21a90b330ee6
rohitaryal <erucix@gmail.com>
2025-06-25 18:47:26 +0530

    add basic routes

---
+++ src/routes/login.ts
@@ -0,0 +1,47 @@
+import { Hono } from "hono";
+import type { Env } from "../types/env.js";
+import { loginValidator } from "../middleware/validators.js";
+import { setCookie } from "../utils/cookie.js";
+import { randomUUID } from "node:crypto";
+import { redirectIfLoggedIn } from "../middleware/auth.js";
+import { hash } from "../utils/hash.js";
+
+const login = new Hono<Env>();
+login.use(redirectIfLoggedIn);
+
+login.post("/", loginValidator, async (c) => {
+    const json = await c.req.json();
+    const sql = c.get("sqlContext");
+
+    json.password = hash(json.password);
+
+    try {
+        // Check if user exists
+        const result = await sql`SELECT * FROM users WHERE email=${json.email} AND hashed_password=${json.password}`;
+
+        if (result.length > 0) {
+            // Random uuid to be used as session. Learnt from `Hevy` :)
+            const uuid = randomUUID();
+
+            // Add session to db
+            await sql`INSERT INTO sessions VALUES(${uuid}, ${json.username})`;
+
+            // Set session to cookie
+            c.res.headers.set("Set-Cookie", setCookie("session", uuid, {
+                httpOnly: true,
+                expires: new Date('2025-12-12'),
+            }));
+            return c.redirect("/", 303);
+        }
+
+        return c.json({ error: "Invalid username or password" }, 401);
+    } catch (err) {
+        return c.json({ error: "Something went wrong!" }, 403);
+    }
+});
+
+login.get("/", async (c) => {
+    return c.text("Looks like you are not logged in!");
+});
+
+export default login;
---
+++ src/routes/register.ts
@@ -0,0 +1,28 @@
+import { Hono } from "hono";
+import type { Env } from "../types/env.js";
+import { registrationValidator } from "../middleware/validators.js";
+import { hash } from "../utils/hash.js";
+import { redirectIfLoggedIn } from "../middleware/auth.js";
+
+const register = new Hono<Env>();
+register.use(redirectIfLoggedIn);
+
+register.post("/", registrationValidator, async (c) => {
+    const json = await c.req.json();
+    json.password = hash(json.password);
+    const sql = c.get("sqlContext");
+
+    try {
+        const existingUsers = await sql`SELECT * FROM users WHERE username=${json.username} OR email=${json.email}`;
+        if (existingUsers.length > 0)
+            return c.json({ error: "Username or email is already used" });
+
+        await sql`INSERT INTO users VALUES(${json.username}, ${json.email}, ${json.password})`;
+
+        return c.redirect("/login");
+    } catch (err) {
+        return c.json({ error: "Something went wrong!" }, 400);
+    }
+});
+
+export default register;
