3ea6d85e49b9c8e5d25ffcfc900c8c462388980c
rohitaryal <erucix@gmail.com>
2025-06-26 04:02:14 +0530

    Add logout route

---
+++ src/routes/logout.ts
@@ -0,0 +1,17 @@
+import { Hono } from "hono";
+import type { Env } from "../types/env.js";
+import { setCookie } from "../utils/cookie.js";
+
+const logout = new Hono<Env>();
+
+logout.all((c) => {
+    c.res.headers.set("Set-Cookie", setCookie("session", "", {
+        expires: new Date(),
+        maxAge: 0,
+        secure: true,
+    }))
+
+    return c.redirect("/login")
+});
+
+export default logout;
