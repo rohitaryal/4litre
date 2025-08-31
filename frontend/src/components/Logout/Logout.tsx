3aedda6fd7dfd9eb2389607a6eb55410ef2dc76e
rohitaryal <erucix@gmail.com>
2025-09-01 04:40:20 +0530

    fix login ui

---
+++ frontend/src/components/Logout/Logout.tsx
@@ -0,0 +1,38 @@
+import { useRef } from "react";
+import styles from "./Logout.module.css";
+import { FiLogOut } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
+
+type LogoutProps = {
+    style?: React.CSSProperties;
+}
+
+const Logout = function (props: LogoutProps) {
+    const navigate = useNavigate();
+    const buttonRef = useRef<HTMLButtonElement>(null);
+
+    const handleClick = async function () {
+        try {
+            const resp = await fetch(import.meta.env.VITE_SERVER_URL + "/logout", { credentials: "include" });
+            if (resp.ok) {
+                navigate("/login");
+            } else {
+                console.log("Failed to log out");
+            }
+        } catch (err) {
+            console.log("Failed to log out: ", err);
+        }
+    }
+
+    return (
+        <button
+            onClick={handleClick}
+            ref={buttonRef}
+            style={props.style}
+            className={styles.logoutButton}>
+            <FiLogOut size={"2em"} />
+        </button>
+    )
+}
+
+export default Logout;
--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,12 +1,26 @@
 import { useEffect } from "react";
 import styles from "./Login.module.css"
 import InputBox from "../../components/InputBox/InputBox";
-import { FiKey, FiMail } from "react-icons/fi";
+import { FiKey, FiMeh } from "react-icons/fi";
 import SplitWindow from "../../components/SplitWindow/SplitWindow";
 import Button from "../../components/Button/Button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
 
 const Login = function () {
+    const navigate = useNavigate();
+    const { result, error } = useFetch(import.meta.env.VITE_SERVER_URL, { credentials: "include" })
+
+    useEffect(() => {
+        if (error) {
+            console.log("Failed to make request");
+        }
+
+        if (result && result.loggedIn) {
+            navigate("/")
+        }
+    }, [navigate, error, result])
+
     useEffect(() => {
         document.title = "Login to 4litre"
     }, []);
@@ -17,14 +31,20 @@ const Login = function () {
                 Log in to <br />
                 <span>4litre</span>
             </span>
-            <form action="/login" method="post" className={styles.loginForm}>
+
+            <form action={import.meta.env.VITE_SERVER_URL + "/login"} method="post" className={styles.loginForm}>
                 <InputBox
-                    type="email"
-                    placeholder="E-mail or Username"
-                    icon={FiMail} />
+                    min={4}
+                    max={20}
+                    type="text"
+                    name="username"
+                    placeholder="Username"
+                    icon={FiMeh}
+                />
                 <br />
                 <InputBox
                     type="password"
+                    name="password"
                     placeholder="Password"
                     icon={FiKey}
                 />
